080e40c31081b4d5c142677c182fad1664dfeb0c
AhmedTalat99 <ahmedmohamed19_sd@fcis.bsu.edu.eg>
2024-10-09 14:59:01 +0300

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -41,7 +41,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "46aed817f67b14638d8b078b7b8af8cf
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "59ee977ee2963bbb48ba873dc3904a7a",
+"flutter_bootstrap.js": "3c1b75f0a569f9fe576b19097f2da5fd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
